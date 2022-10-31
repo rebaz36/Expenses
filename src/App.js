4f8d00873bc55f20b645f900b64783273d4147a2
rebaz36 <rebaz415@gmail.com>
2022-10-31 12:00:59 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -23,6 +24,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
