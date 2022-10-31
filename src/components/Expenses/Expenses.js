af24ad9741e3e54f912cc07f640f3c429b638938
rebaz36 <rebaz415@gmail.com>
2022-10-31 15:46:39 +0300

    Manage Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,44 @@
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/card/card";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+
 export default function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("");
+  const handleFilter = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          Title={expense.title}
-          Amount={expense.amount}
-          Date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter handleFilter={handleFilter} />
+        {filteredYear === ""
+          ? expenses.map((expense) => {
+              return (
+                <ExpenseItem
+                  key={expense.id}
+                  Title={expense.title}
+                  Amount={expense.amount}
+                  Date={expense.date}
+                />
+              );
+            })
+          : filteredExpenses.map((expense) => {
+              return (
+                <ExpenseItem
+                  key={expense.id}
+                  Title={expense.title}
+                  Amount={expense.amount}
+                  Date={expense.date}
+                />
+              );
+            })}
+      </Card>
+    </div>
   );
 }
